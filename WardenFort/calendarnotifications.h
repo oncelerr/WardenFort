a6efa68108879291928353c3380fc0bbefaad2a4
bibumu <161674166+bibumu@users.noreply.github.com>
2024-06-03 13:37:48 +0800

    hhhh

--- WardenFort/calendarnotifications.h
+++ WardenFort/calendarnotifications.h
@@ -2,12 +2,15 @@
 #define CALENDARNOTIFICATIONS_H
 
 #include <QMainWindow>
-#include <QMap>
 #include <QListWidgetItem>
+#include <QMap>
+#include <QPair>
+#include <QDate>
+#include <QPushButton>
 
-namespace Ui {
-class CalendarNotifications;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class CalendarNotifications; }
+QT_END_NAMESPACE
 
 class CalendarNotifications : public QMainWindow
 {
@@ -17,12 +20,25 @@ public:
     explicit CalendarNotifications(QWidget *parent = nullptr);
     ~CalendarNotifications();
 
-    void addNotification(const QString &date, const QString &time, const QString &text, bool isImportant);
-    void updateNotifications();
+private slots:
+    void onTriButtonClicked();
+    void onTriReversedButtonClicked();
+    void onProfilePushButtonClicked();
+    void onProfileLessButtonClicked();
+    void onAlertPushButtonClicked();
+    void onAlertLessButtonClicked();
+    void onReportPushButtonClicked();
+    void onReportLessButtonClicked();
 
 private:
     Ui::CalendarNotifications *ui;
-    QMap<QString, QList<QPair<QString, QString>>> notifications;  // Date -> List of (time, text)
+    QMap<QDate, QList<QPair<QString, QPair<QString, bool>>>> notifications; // Modified to include isImportant
+
+    void addNotification(const QString &date, const QString &time, const QString &text, bool isImportant);
+    void updateNotifications();
+    void applyDateItemStyles();
+    void toggleButtonVisibility(QPushButton* buttonToHide, QPushButton* buttonToShow);
+    void hideSpecifiedButtons();  // Declare this function if it is used
 };
 
 #endif // CALENDARNOTIFICATIONS_H
